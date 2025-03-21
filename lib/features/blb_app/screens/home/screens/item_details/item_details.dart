1207efb31667a32fe8e9218bbb241448ca865ab2
pahojosue <pahotchaptchet4@gmail.com>
2025-03-21 13:58:09 +0100

    changes to item details

--- lib/features/blb_app/screens/home/screens/item_details/item_details.dart
+++ lib/features/blb_app/screens/home/screens/item_details/item_details.dart
@@ -1,7 +1,5 @@
 import 'package:blb/common/widgets/appbar/appbar.dart';
-import 'package:blb/common/widgets/custom_shapes/containers/rounded_container.dart';
 import 'package:blb/common/widgets/icons/blb_circular_icon.dart';
-import 'package:blb/common/widgets/images/blb_rounded_image.dart';
 import 'package:blb/utils/constants/colors.dart';
 import 'package:blb/utils/constants/image_strings.dart';
 import 'package:blb/utils/constants/sizes.dart';
@@ -40,7 +38,7 @@ class ItemDetails extends StatelessWidget {
                   children: [
                     //Main Large Image
                     const SizedBox(
-                        height: 400,
+                        height: 300,
                         child: Padding(
                           padding:
                               EdgeInsets.all(BLBSizes.productImageRadius * 2),
@@ -48,32 +46,6 @@ class ItemDetails extends StatelessWidget {
                               child:
                                   Image(image: AssetImage(BLBImages.appLogo))),
                         )),
-
-                    //Image Slider
-                    Positioned(
-                      right: 0,
-                      bottom: 30,
-                      left: BLBSizes.defaultSpace,
-                      child: SizedBox(
-                        height: 80,
-                        child: ListView.separated(
-                          itemCount: 6,
-                          shrinkWrap: true,
-                          scrollDirection: Axis.horizontal,
-                          physics: const AlwaysScrollableScrollPhysics(),
-                          separatorBuilder: (_, __) =>
-                              const SizedBox(width: BLBSizes.spaceBtwItems),
-                          itemBuilder: (_, index) => BLBRoundedImage(
-                            width: 80,
-                            backgroundColor:
-                                isDark ? BLBColors.dark : BLBColors.white,
-                            border: Border.all(color: BLBColors.primary),
-                            padding: const EdgeInsets.all(BLBSizes.sm),
-                            imageUrl: BLBImages.appLogo,
-                          ),
-                        ),
-                      ),
-                    ),
 
                     //Appbar Icons
                     BLBAppBar(
@@ -99,120 +71,69 @@ class ItemDetails extends StatelessWidget {
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  //Rating & share
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      //Rating
-                      Row(
-                        children: [
-                          Icon(Iconsax.star5, color: Colors.amber, size: 24),
-                          SizedBox(width: BLBSizes.spaceBtwItems / 2),
-                          Text.rich(TextSpan(children: [
-                            TextSpan(
-                                text: '5.0',
-                                style: Theme.of(context).textTheme.bodyLarge),
-                            const TextSpan(text: '(199)'),
-                          ]))
-                        ],
-                      ),
-                      //Share button
-                      IconButton(
-                          onPressed: () {},
-                          icon: const Icon(Icons.share, size: BLBSizes.iconMd)),
-                    ],
-                  ),
-
                   //Price, title, stock, brand
                   Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      //Price & Sale Price
-                      Row(
-                        children: [
-                          //Sale Tag
-                          BLBRoundedContainer(
-                            radius: BLBSizes.sm,
-                            backgroundColor:
-                                BLBColors.secondary.withValues(alpha: 0.8),
-                            padding: const EdgeInsets.symmetric(
-                                horizontal: BLBSizes.sm, vertical: BLBSizes.xs),
-                            child: Text('25%',
-                                style: Theme.of(context)
-                                    .textTheme
-                                    .labelLarge!
-                                    .apply(color: BLBColors.black)),
-                          ),
-                          const SizedBox(width: BLBSizes.spaceBtwItems),
-
-                          //Price
-                          Text('\$250',
-                              style: Theme.of(context)
-                                  .textTheme
-                                  .titleSmall!
-                                  .apply(
-                                      decoration: TextDecoration.lineThrough)),
-                          const SizedBox(width: BLBSizes.spaceBtwItems),
-                          Text('\$175',
-                              style: Theme.of(context).textTheme.titleSmall)
-                        ],
-                      ),
-                      const SizedBox(width: BLBSizes.spaceBtwItems / 1.5),
-
                       //Title
-                      Text('A good Logo'),
-                      const SizedBox(width: BLBSizes.spaceBtwItems / 1.5),
+                      Center(
+                        child: Text('<<Item Name>>',
+                            style: Theme.of(context).textTheme.headlineMedium),
+                      ),
+                      const SizedBox(height: BLBSizes.spaceBtwItems),
 
-                      //Stock status
+                      Text("<<Lender Name>>",
+                          overflow: TextOverflow.ellipsis,
+                          maxLines: 1,
+                          style: Theme.of(context).textTheme.headlineSmall),
+
+                      const SizedBox(height: BLBSizes.spaceBtwItems),
                       Row(
                         children: [
-                          Text('Status'),
-                          const SizedBox(width: BLBSizes.spaceBtwItems),
-                          Text("In stock"),
+                          Text('<<Address:>>',
+                              style: Theme.of(context).textTheme.headlineSmall),
+                          const SizedBox(width: BLBSizes.spaceBtwItems / 1.5),
+                          Text('<<Address>>',
+                              style: Theme.of(context).textTheme.headlineSmall),
                         ],
                       ),
-                      const SizedBox(width: BLBSizes.spaceBtwItems / 1.5),
-
-                      //Brand
+                      const SizedBox(height: BLBSizes.spaceBtwItems),
+                      //Item status
                       Row(
                         children: [
-                          Text("Logo",
-                              overflow: TextOverflow.ellipsis,
-                              maxLines: 1,
-                              style: Theme.of(context).textTheme.labelLarge),
-                          const SizedBox(width: BLBSizes.xs),
-                          const Icon(Iconsax.verify5,
-                              color: BLBColors.primary, size: BLBSizes.iconXs),
+                          Text('<<Item State : >>',
+                              style: Theme.of(context).textTheme.headlineSmall),
+                          const SizedBox(width: BLBSizes.spaceBtwItems / 1.5),
+                          Text('<<Fairly used>>',
+                              style: Theme.of(context).textTheme.headlineSmall),
                         ],
                       ),
+                      const SizedBox(height: BLBSizes.spaceBtwItems),
+                      Text("Minimum Lending Period :" " <<>>",
+                          overflow: TextOverflow.ellipsis,
+                          maxLines: 1,
+                          style: Theme.of(context).textTheme.headlineSmall),
+
+                      const SizedBox(height: BLBSizes.spaceBtwItems),
+                      Text("Price per day :" " XAF",
+                          overflow: TextOverflow.ellipsis,
+                          maxLines: 1,
+                          style: Theme.of(context).textTheme.headlineSmall),
                     ],
                   ),
 
                   const SizedBox(height: BLBSizes.spaceBtwSections),
-
-                  //Description
-                  Text(
-                    "Description",
-                    style: Theme.of(context).textTheme.headlineMedium,
-                    textAlign: TextAlign.left,
+                  Center(
+                    child: Text("Description",
+                        style: Theme.of(context).textTheme.headlineMedium),
                   ),
-                  const SizedBox(height: BLBSizes.spaceBtwItems),
-                  const Text("This is a good Logo"),
-
-                  //Reviews
-                  const Divider(),
-                  const SizedBox(height: BLBSizes.spaceBtwItems),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text("Reviews(199)",
-                          style: Theme.of(context).textTheme.headlineMedium),
-                      IconButton(
-                          onPressed: () {},
-                          icon: const Icon(Iconsax.arrow_right_3, size: 18)),
-                    ],
-                  ),
-                  const SizedBox(height: BLBSizes.spaceBtwSections),
+                  const SizedBox(
+                      height: 75,
+                      child: Text(
+                        "",
+                        overflow: TextOverflow.ellipsis,
+                        maxLines: 1,
+                      )),
                 ],
               ),
             ),
